2f4075f73ca8794bd50d7270ff82c9bc7b3d58cf
Beatriz Favini Chicaroni <bia.f.chicaroni@uol.com.br>
2021-11-14 18:46:31 -0300

    Primeira conexão do bak com o front

--- route.js
+++ route.js
@@ -1,10 +1,16 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(express.urlencoded({ extended: true}))
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
 
-app.get("/home", function(req, res) {
-    res.send("Olá manas");
+app.post("/novoCadastro", function(req, res) {
+    res.send("Olá, " + req.body.nome + " seu cadastro foi efetuado com sucesso!");
 });
 
+app.get("/", function(req, res) {
+    res.send("Rodando");
+});
