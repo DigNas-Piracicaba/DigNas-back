662a00dcd74eb53ebf1213763e1c202e49a02121
Beatriz Favini Chicaroni <bia.f.chicaroni@uol.com.br>
2021-12-06 18:37:26 -0300

    Implementação completa da API novoCadastro

--- Cadastro/model/user.js
+++ Cadastro/model/user.js
@@ -2,7 +2,27 @@ const mongoose = require('mongoose');
 const connection = mongoose.createConnection('mongodb://localhost:27017/DigNas');
 
 const schema = mongoose.Schema({
-    nome: String
+    nome: String,
+    dtNasc: String,
+    genero: String,
+    etnia: String,
+    cep: String,
+    logradouro: String,
+    numero: String,
+    complemento: String,
+    bairro: String,
+    cidade: String,
+    estado: String,
+    telefone: String,
+    celular: String,
+    email: String,
+    estadoCivil: String,
+    escolaridade: String,
+    apoioFinanceiro: String,
+    redeCelular: Boolean,
+    distCidade: Number,
+    filhos: Boolean,
+    qtdeFilhos: Number    
 }, {
     collection: 'usuarios'
 });
